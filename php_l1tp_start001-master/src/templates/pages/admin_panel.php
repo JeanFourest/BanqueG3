f1b9da5288b051270fa6ddd04e1802b22ee397ee
FOUREST <jmfourest@gmail.com>
2023-01-12 11:30:19 +0100

    save

---
+++ php_l1tp_start001-master/src/templates/pages/admin_panel.php
@@ -0,0 +1,13 @@
+<?php
+
+$page_title = "Admin panel";
+
+ob_start();
+?>
+<h1>Edit roles</h1>
+
+
+
+<?php
+
+$page_content = ob_get_clean();
--- php_l1tp_start001-master/src/templates/partials/menu.php
+++ php_l1tp_start001-master/src/templates/partials/menu.php
@@ -9,7 +9,25 @@
 	<li><a href="/actions/logout.php">Logout</a></li>
 <?php } ?>
 </ul>
-<?php } if ($user->role >= $role['lvl']['manager']){ ?>
+
+
+<?php } if ($user->role == $role['lvl']['admin']){ ?>
+<ul>
+	<li><a href="/?page=home">Home</a></li>
+	<li><a href="/?page=contact">Contact</a></li>
+<?php if ($user === false) { ?>
+	<li><a href="/?page=signup">Signup</a></li>
+	<li><a href="/?page=login">Login</a></li>
+<?php } else { ?>
+	<li><?= $user->email; ?></li>
+	<li><a href="/actions/logout.php">Logout</a></li>
+<?php } ?>
+	<li><a href="/?page=admin_contact">Admin Contact</a></li>
+	<li><a href="/?page=admin_panel">Admin Panel</a></li>
+</ul>
+
+
+<?php } if ($user->role == $role['lvl']['manager']){ ?>
 <ul>
 	<li><a href="/?page=home">Home</a></li>
 	<li><a href="/?page=contact">Contact</a></li>
